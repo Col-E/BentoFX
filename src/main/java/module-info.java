6bf2538f3704af1b82091c7a382409e10d7a3fe2
Matt <mcoley2@gmu.edu>
2025-04-26 18:55:10 -0400

    Fix module-info not exporting anything
    If it wasn't clear I don't ever use JPMS

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,21 @@
 module bento.fx {
-	requires jakarta.annotation;
+	requires static jakarta.annotation;
 
 	requires javafx.base;
 	requires javafx.graphics;
 	requires javafx.controls;
 
-	// Just open everything. Do whatever you want.
+	// Just open/export everything. Do whatever you want.
+	exports software.coley.bentofx;
+	exports software.coley.bentofx.builder;
+	exports software.coley.bentofx.content;
+	exports software.coley.bentofx.header;
+	exports software.coley.bentofx.impl;
+	exports software.coley.bentofx.impl.content;
+	exports software.coley.bentofx.impl.layout;
+	exports software.coley.bentofx.layout;
+	exports software.coley.bentofx.path;
+	exports software.coley.bentofx.util;
 	opens software.coley.bentofx;
 	opens software.coley.bentofx.builder;
 	opens software.coley.bentofx.content;
