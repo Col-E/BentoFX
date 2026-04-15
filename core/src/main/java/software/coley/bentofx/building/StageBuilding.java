7ae07394a9cd8e125f4b8825761082c12c10830b
Phil Bryant <phillip.l.bryant@saic.com>
2026-04-15 14:59:47 -0500

    BENTO-17: * Remove unused method.

--- core/src/main/java/software/coley/bentofx/building/StageBuilding.java
+++ core/src/main/java/software/coley/bentofx/building/StageBuilding.java
@@ -1,7 +1,6 @@
 package software.coley.bentofx.building;
 
 import javafx.geometry.Point2D;
-import javafx.geometry.Side;
 import javafx.scene.Scene;
 import javafx.scene.layout.Region;
 import javafx.scene.robot.Robot;
@@ -14,7 +13,6 @@ import software.coley.bentofx.control.DragDropStage;
 import software.coley.bentofx.dockable.Dockable;
 import software.coley.bentofx.layout.DockContainer;
 import software.coley.bentofx.layout.container.DockContainerLeaf;
-import software.coley.bentofx.layout.container.DockContainerLeafMenuFactory;
 import software.coley.bentofx.layout.container.DockContainerRootBranch;
 
 /**
@@ -160,40 +158,6 @@ public class StageBuilding {
 		}
 
 		return stage;
-	}
-
-	/**
-	 * Create a new stage for the given dockable.,
-	 *
-	 * @param sourceScene
-	 * 		Original scene to copy state from.
-	 * @param dockable
-	 * 		Dockable to place into the newly created stage.
-	 * @param width
-	 * 		Preferred stage width.
-	 * @param height
-	 * 		Preferred stage height.
-	 * @param leafMenuFactory
-	 * 		DockContainerLeafMenuFactory for creating leaf menus
-	 * @param side
-	 * 		Side of this container to place {@code Header} displays on.
-	 *        {@code null} to not display any headers.
-	 *
-	 * @return Newly created stage.
-	 */
-	@NonNull
-	public DragDropStage newStageForDockable(@Nullable Scene sourceScene,
-	                                         @NonNull Dockable dockable,
-	                                         double width,
-	                                         double height,
-	                                         @Nullable DockContainerLeafMenuFactory leafMenuFactory,
-	                                         @Nullable Side side) {
-		DockBuilding builder = bento.dockBuilding();
-		DockContainerRootBranch root = builder.root();
-		DockContainerLeaf leaf = builder.leaf();
-		leaf.setMenuFactory(leafMenuFactory);
-		leaf.setSide(side);
-		return newStageForDockable(sourceScene, root, leaf, dockable, width, height);
 	}
 
 	/**
